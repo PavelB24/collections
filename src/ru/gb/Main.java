905ad5eef78a13e977a98560bd60dc204bbd3e7d
Pavel <alkatrazzzzz@mail.ru>
2021-08-07 17:58:30 +0300

    v0.1

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -19,13 +19,14 @@ public class Main {
         // Оптимальный вариант, но я не совсем понимаю, как он работает, а потому оставлю его до момента более детального изучения потоков
         System.out.println(animals.stream().collect(Collectors.groupingBy(k -> k, Collectors.counting())));
         HashMap<String, Integer> mapForList = new HashMap<>();
-        Set<String> animalSet = new HashSet<>();
+//        Set<String> animalSet = new HashSet<>();
         for (String animal : animals) {
             mapForList.put(animal, Collections.frequency(animals, animal));
         }
         for (String s : mapForList.keySet()) {
             System.out.println(s + " повторяется в коллекции animals " + mapForList.get(s) + " раз");
         }
+
 
         PhoneDirectory directory = new PhoneDirectory(new HashMap());
         directory.addPhones("Баринов", "89992476337");
